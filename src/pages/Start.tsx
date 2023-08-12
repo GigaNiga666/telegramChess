5c8e371c2e3fca50c82b778df8ba3831e2ab5053
Stanislav <stasbugaev2006@gmail.com>
2023-08-12 22:29:17 +0500

    init

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -3,6 +3,14 @@ import {Link} from "react-router-dom";
 const { v4 } = require('uuid');
 
 const Start = () => {
+
+    useEffect(() => {
+        fetch('https://95f8-79-172-16-96.ngrok-free.app')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(e => console.log(e))
+    }, [])
+
     return (
         <Link to={`/game/${v4()}`}>
             <button type='button'>Play</button>
