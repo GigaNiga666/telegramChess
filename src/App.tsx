06c4d6a5fb550f7250a3c47efa8cbd4df72a56d3
Stanislav <stasbugaev2006@gmail.com>
2023-08-15 14:26:10 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Scene from "./pages/Scene";
-import Start from "./pages/Start";
 import {useTelegram} from "./hooks/useTelegram";
 
 
@@ -22,7 +21,6 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/game/:id' element={<Scene/>}/>
-                <Route path='/' element={<Start/>}/>
             </Routes>
         </BrowserRouter>
     );
